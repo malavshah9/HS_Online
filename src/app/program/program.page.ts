b0869060f8b5468cd0d7768b95f050cd11d0a2c5
malavshah9 <mdshah9574@gmail.com>
2020-02-06 00:15:41 +0530

    timer added

--- src/app/program/program.page.ts
+++ src/app/program/program.page.ts
@@ -11,15 +11,17 @@ import { UserDbService } from '../services/user-db.service';
 export class ProgramPage implements OnInit {
   userName:String;
   userBalance:String="0";
-  // draw_hour:number=0;
-  // draw_minute:number=0;
-  // am_or_pm:string="";
-  // current_date:string="";
-  // current_hour:number=0;
-  // current_minute:number=0;
-  // current_second:number=0;
-  // remaining_minute:number=0;
-  // remaining_second:number=0;
+  draw_hour:string="0";
+  draw_minute:string="0";
+  am_or_pm:string="";
+  current_date:string="0";
+  current_hour:string="0";
+  current_minute:string="0";
+  current_second:string="0";
+  current_time;
+  draw_time;
+  remaining_minute:string="";
+  remaining_second:string="";
   // dateInterval:number=0;
   constructor(public route: Router,private statusBar: StatusBar,private screenOrientation: ScreenOrientation,private userDb:UserDbService) { 
     this.userName=localStorage.getItem('UserName');
@@ -31,64 +33,81 @@ export class ProgramPage implements OnInit {
   }
 
   ngOnInit() {
-    // this.screenOrientation.lock(this.screenOrientation.ORIENTATIONS.LANDSCAPE);
-    // this.setDrawTimer();
-    // this.setCurrentDate();
-    // this.setCurrentTime();
-    // setInterval(()=>{
-    //   this.setCurrentTime();
-    // },1000);
-    // setInterval(()=>{
-    //   this.setDrawTimer();
-    // },this.remaining_minute*1000);
-    // setInterval(()=>{
-    //   this.setCurrentDate();
-    // },this.dateInterval)
+    this.setTime();
+    setInterval(()=>{
+      this.setTime();
+    },1000);
+   
   }
-  // setDrawTimer(){
-  //   let today=new Date();
-  //   let minute=today.getMinutes();
-  //   if(minute>=45 && minute<=59){
-  //     this.draw_hour=this.addHours(today,1);
-  //   }
-  //   else{
-  //     this.draw_hour=today.getHours();
-  //   }
-  //   this.am_or_pm=this.draw_hour>=12?"PM":"AM";
-  //   let number_hour=+this.draw_hour;
-  //   number_hour=number_hour%12;
-  //   number_hour=number_hour?number_hour:12;
-  //   // this.draw_hour=number_hour<=9?'0'+number_hour.toString():number_hour.toString();
-  //   console.log(" this.draw_hour ",this.draw_hour);
-  //   let currentMinutes=today.getMinutes();
-  //   if(currentMinutes>=0 && currentMinutes<=14){
-  //     this.draw_minute=15;
-  //   }
-  //   else if(currentMinutes>=15 && currentMinutes<=29){
-  //     this.draw_minute=30;
-  //   }
-  //   else if(currentMinutes>=30 && currentMinutes<=44){
-  //     this.draw_minute=45;
-  //   }
-  //   else{
-  //     this.draw_minute=0;
-  //   }
-  // }
-  // setCurrentTime(){
-  //   let date = new Date();
-  //   let hours = date.getHours() < 10 ? "0" + date.getHours() : date.getHours();
-  //   let minutes = date.getMinutes() < 10 ? "0" + date.getMinutes() : date.getMinutes();
-  //   let seconds = date.getSeconds() < 10 ? "0" + date.getSeconds() : date.getSeconds();
-  //   hours=hours%12;
-  //   hours=hours?hours:12;
-  //   // this.current_hour=hours<=9?'0'+hours.toString():hours.toString();
-  //   // this.current_minute=minutes.toString();
-  //   // this.current_second=seconds.toString();
-  //   let number_draw_minute=+this.draw_minute;
-  //   let remain_minute=number_draw_minute-minutes;
-  //   this.remaining_minute=remain_minute;
-  //   // this.remaining_second="";
-  // }
+  setTime(){
+    this.setDrawTimer();
+    this.setCurrentTime();
+    this.setDifferenceTime();
+    this.setCurrentDate();
+  }
+  setCurrentDate(){
+    let date:any=new Date().getDate();
+    date=date<=9?"0"+date.toString():date.toString();
+    let month:any=new Date().getUTCMonth()+1;
+    month=month<=9?"0"+month.toString():month.toString();
+    let year:any=new Date().getFullYear();
+    this.current_date=date+"-"+month+"-"+year;
+  }
+  setDifferenceTime(){
+    let delta:any = Math.abs(this.draw_time - this.current_time) / 1000;
+    let days = Math.floor(delta / 86400);
+    delta -= days * 86400;
+    let hours = Math.floor(delta / 3600) % 24;
+    delta -= hours * 3600;
+    let minutes = Math.floor(delta / 60) % 60;
+    delta -= minutes * 60;
+    let seconds = delta % 60;
+    this.remaining_minute=minutes<=9?"0"+minutes:minutes.toString();
+    this.remaining_second=seconds<=9?"0"+seconds:seconds.toString();
+  }
+  setDrawTimer(){
+    let today=new Date();
+    let minute=today.getMinutes();
+    if(minute>=45 && minute<=59){
+      this.draw_hour=new Date(new Date().setHours(new Date().getHours()+1)).getHours().toString();
+      // this.draw_hour=this.addHours(today,1);
+    }
+    else{
+      this.draw_hour=today.getHours().toString();
+    }
+    let draw_hour_number=+this.draw_hour;
+    
+    this.am_or_pm=draw_hour_number>=12?"PM":"AM";
+    let number_hour=+this.draw_hour;
+    number_hour=number_hour%12;
+    number_hour=number_hour?number_hour:12;
+    this.draw_hour=number_hour<=9?"0"+number_hour+"":number_hour.toString();
+    let currentMinutes=today.getMinutes();
+    if(currentMinutes>=0 && currentMinutes<=14){
+      this.draw_minute="15";
+    }
+    else if(currentMinutes>=15 && currentMinutes<=29){
+      this.draw_minute="30";
+    }
+    else if(currentMinutes>=30 && currentMinutes<=44){
+      this.draw_minute="45";
+    }
+    else{
+      this.draw_minute="00";
+    }
+    let draw_minute_number=+this.draw_minute;
+    this.draw_time=new Date().setHours(draw_hour_number,draw_minute_number,0);
+  }
+  setCurrentTime(){
+    let date = new Date();
+    let hours=date.getHours();
+    hours=hours%12;
+    hours=hours?hours:12;
+    this.current_hour=hours<=9?'0'+hours.toString():hours.toString();
+    this.current_minute=date.getMinutes()<10?"0"+date.getMinutes().toString():date.getMinutes().toString();
+    this.current_second=date.getSeconds()<10?"0"+date.getSeconds().toString():date.getSeconds().toString();
+    this.current_time=date.getTime();
+  }
   // setCurrentDate(){
   //   let today = new Date(); 
   //   let dd = today.getDate(); 
@@ -107,12 +126,11 @@ export class ProgramPage implements OnInit {
   //   console.log("dateInterval ",this.dateInterval);
 
   // }
-  // addHours(date:Date,hour){
-  //   let anotherDay=date.setTime(date.getTime()+(hour*60*60*100));
-  //   let dateAnother:Date=new Date(anotherDay);
-  //   console.log(" anotherDay ",anotherDay);
-  //   return dateAnother.getHours().toString();
-  // }
+  addHours(date:Date,hour){
+    let anotherDay=date.setTime(date.getTime()+(hour*60*60*100));
+    let dateAnother:Date=new Date(anotherDay);
+    return dateAnother.getHours().toString();
+  }
   ionViewWillEnter(){
     this.statusBar.hide();
     // this.statusBar.overlaysWebView(true);
