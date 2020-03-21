260fa0675bc1f8a17e616dbf2dc9ac5f0475213d
Zeel Modi <zeelmodi@quarkstudios.com>
2020-03-22 00:43:04 +0530

    ios build added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,11 @@
 import { Component, ViewChildren, QueryList } from '@angular/core';
-import { Platform, ModalController, IonRouterOutlet, NavController, AlertController } from '@ionic/angular';
+import {
+  Platform,
+  ModalController,
+  IonRouterOutlet,
+  NavController,
+  AlertController
+} from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 // import { SplashPage } from './splash/splash.page';
@@ -10,10 +16,9 @@ import { UserDbService } from './services/user-db.service';
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss']
 })
-
 export class AppComponent {
-  appVersion="";
-  
+  appVersion = '';
+
   @ViewChildren(IonRouterOutlet) routerOutlets: QueryList<IonRouterOutlet>;
   constructor(
     private platform: Platform,
@@ -21,39 +26,49 @@ export class AppComponent {
     private statusBar: StatusBar,
     private navCtrl: NavController,
     private alertController: AlertController,
-    private userDb:UserDbService,
+    private userDb: UserDbService,
     // public modalCtrl: ModalController,
-     public route:Router ) {
+    public route: Router
+  ) {
+    this.splashScreen.hide();
     this.initializeApp();
-    
   }
   initializeApp() {
     this.platform.ready().then(() => {
       this.statusBar.hide();
     });
-    
-    this.userDb.getVersion().subscribe(async (data)=>{
-      this.appVersion=data[0].AppVersionId;
-    },(err)=>{
-      console.log(err);
-    },async ()=>{
-      if(this.appVersion!=="1"){
-        const battingAlert = await this.alertController.create({
-          buttons: [{text:'OK',cssClass:'my-alert-button',handler:()=>{
+
+    this.userDb.getVersion().subscribe(
+      async data => {
+        this.appVersion = data[0].AppVersionId;
+      },
+      err => {
+        console.log(err);
+      },
+      async () => {
+        if (this.appVersion !== '1') {
+          const battingAlert = await this.alertController.create({
+            buttons: [
+              {
+                text: 'OK',
+                cssClass: 'my-alert-button',
+                handler: () => {
+                  navigator['app'].exitApp();
+                }
+              }
+            ],
+            mode: 'ios',
+            message: 'Your app is outdated.Please ask for new app.'
+          });
+          battingAlert.present().then(() => {
             navigator['app'].exitApp();
-          }}],
-          mode:'ios',
-          message:"Your app is outdated.Please ask for new app."
-        });
-        battingAlert.present().then(()=>{
-          navigator['app'].exitApp();
-        })
+          });
+        }
       }
-    });
-    if(localStorage.getItem('UserId')!=null){
+    );
+    if (localStorage.getItem('UserId') != null) {
       this.route.navigate(['dashboard']);
-    }
-    else{
+    } else {
       this.route.navigate(['home']);
     }
   }
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -4,70 +4,82 @@ import { Component, ViewChild } from '@angular/core';
 import { Router } from '../../../node_modules/@angular/router';
 import { UserDbService } from '../services/user-db.service';
 import { ToastController, LoadingController } from '@ionic/angular';
-import { FormGroup, FormBuilder, FormControl, Validators } from "@angular/forms";
+import {
+  FormGroup,
+  FormBuilder,
+  FormControl,
+  Validators
+} from '@angular/forms';
 import { User } from '../shared/User_class';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  styleUrls: ['home.page.scss']
 })
 export class HomePage {
   loginForm: FormGroup;
-  user:User;
-  constructor(public route: Router,private useDb:UserDbService,public toast: ToastController,public formBuilder: FormBuilder,
-    private loadingController:LoadingController) {
+  user: User;
+  constructor(
+    public route: Router,
+    private useDb: UserDbService,
+    public toast: ToastController,
+    public formBuilder: FormBuilder,
+    private loadingController: LoadingController
+  ) {
+    console.log('login page loaded');
     this.loginForm = this.formBuilder.group({
-      'username': new FormControl('', Validators.compose([
-        Validators.required
-      ])),
-      'password': new FormControl('', Validators.compose([
-        Validators.required
-      ]))
+      username: new FormControl('', Validators.compose([Validators.required])),
+      password: new FormControl('', Validators.compose([Validators.required]))
     });
-    if(!isNull(localStorage.getItem("UserId"))){
+    if (!isNull(localStorage.getItem('UserId'))) {
       this.route.navigateByUrl('/dashboard');
     }
-   }
+  }
   async onClick() {
     // this.route.navigateByUrl('/dashboard');
     const loading = await this.loadingController.create({
       message: 'Logging in...',
-      duration:5000
+      duration: 5000
     });
     await loading.present();
-    this.user=new User(this.loginForm.get('username').value,this.loginForm.get('password').value);
-    this.useDb.loginUser(this.user).subscribe(async (data:any)=>{
-      let t1:HTMLIonToastElement;
-      console.log(data);
-      if(data.result) {
-        t1 = await this.toast.create({
-          message: "Successfull Login!",
-          duration: 1000,
-          showCloseButton: true,
-          closeButtonText: 'Okay',
-          cssClass: 'myClass'
-        });
-        localStorage.setItem('UserId',data.UserId);
-        localStorage.setItem('UserName',data.UserName);
-        this.route.navigateByUrl('/dashboard');
-      }
-      else {
-        t1 = await this.toast.create({
-          message: "Invalid User Name or Password!",
-          duration: 1000,
-          showCloseButton: true,
-          closeButtonText: 'Okay',
-          cssClass: 'myClass'
-        });
-      }
-      this.loginForm.reset();
-      t1.present();
-    },err=>{
-      alert(err.message);
-      console.log(err);
-    },()=>{
+    this.user = new User(
+      this.loginForm.get('username').value,
+      this.loginForm.get('password').value
+    );
+    this.useDb.loginUser(this.user).subscribe(
+      async (data: any) => {
+        let t1: HTMLIonToastElement;
+        console.log(data);
+        if (data.result) {
+          t1 = await this.toast.create({
+            message: 'Successfull Login!',
+            duration: 1000,
+            showCloseButton: true,
+            closeButtonText: 'Okay',
+            cssClass: 'myClass'
+          });
+          localStorage.setItem('UserId', data.UserId);
+          localStorage.setItem('UserName', data.UserName);
+          this.route.navigateByUrl('/dashboard');
+        } else {
+          t1 = await this.toast.create({
+            message: 'Invalid User Name or Password!',
+            duration: 1000,
+            showCloseButton: true,
+            closeButtonText: 'Okay',
+            cssClass: 'myClass'
+          });
+        }
+        this.loginForm.reset();
+        t1.present();
+      },
+      err => {
+        alert(err.message);
+        console.log(err);
+      },
+      () => {
         loading.dismiss();
-    });
+      }
+    );
   }
- 
 }
